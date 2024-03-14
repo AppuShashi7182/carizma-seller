a70026e6ebc3fced9019e607b9de4bb18b2de7a7
Tarun K <tarun.k@knowledgelens.com>
2024-03-14 10:56:30 +0530

    Minor updates

--- src/app/navigation/footer/footer.component.ts
+++ src/app/navigation/footer/footer.component.ts
@@ -12,6 +12,10 @@ export class FooterComponent {
   constructor(private router: Router) { }
 
   navigate(route:string) {
+    if(this.router.url?.split('/').includes('questionaire') && route === 'terms-and-conditions'){
+      window.open('https://www.carizma.com/terms-and-conditions', '_blank');
+      return;
+    }
     this.router.navigate([`/${route}`]);
   }
 }
